aff7678db7f75ce1aabdb246f9166cd765ce9a9e
wathika-eng <wathika02@gmail.com>
2024-03-17 18:02:37 +0300

    ...clone

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = new Object();
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  employee = { ...employee };
+  employee[key] = value;
+  return employee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  employee = { ...employee };
+  delete employee[key];
+  return employee;
+}
+deleteFromEmployeeByKey();
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
